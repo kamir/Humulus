17c0a3fba2348437a57688b6e4f80c2910f646c6
Mirko Kämpf <mirko.kaempf@cloudera.de>
2014-06-12 17:13:19 +0100

    initial commit

---
+++ HumulusPOC/src/app/HumulusPOC.java
@@ -0,0 +1,91 @@
+/**
+ *  This Tool will demo how some DOAx files are adds to a scalable triple store
+ *  in order to query those via CumulusRDF web application. 
+ *
+ **/
+
+package app;
+
+/**
+ *
+ * @author kamir
+ */
+public class HumulusPOC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println( ">>> There is still a lot todo ... ;-)");
+    
+        prepare();
+        
+        runOnCasandra();
+        
+        runOnHBase();
+        
+        runOnFuseki();
+        
+        runOnSOLR();
+    }
+
+    private static void runOnCasandra() {
+
+        // load some DOAP files ....
+        
+        
+        // load some DOAD files ...
+        
+        
+        // query some stuff
+
+    
+    }
+
+    private static void runOnHBase() {
+    
+        // load some DOAP files ....
+        
+        
+        // load some DOAD files ...
+        
+        
+        // query some stuff
+
+    
+    }
+
+    private static void runOnFuseki() {
+    
+        // load some DOAP files ....
+        
+        
+        // load some DOAD files ...
+        
+        
+        // query some stuff
+
+    }
+
+    private static void runOnSOLR() {
+    
+        // load some DOAP files ....
+        
+        
+        // load some DOAD files ...
+        
+        
+        // query some stuff
+
+    
+    }
+
+    String path2DOAP = null;
+    String path2DOAD = null;
+    String query = null;
+    
+    private static void prepare() {
+        
+    }
+    
+}
